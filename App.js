e9731055c0fa97602dbe15a7bdbe67d49ce686ba
Domaya <c2hila@gmail.com>
2021-08-10 21:33:12 +0900

    로딩 화면, 위치 얻어오기

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Text } from 'react-native';
+import Loading from "./Loading";
+import * as Location from 'expo-location';
+import axios from "axios";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = "17d45cbf0899e73885be9901ec9de7be";
+
+export default class extends React.Component {
+  state = {
+    isLoading : true
+  }
+  // getWeather = async(){
+
+  // }
+  getLocation = async() => {
+    try {
+      await Location.requestForegroundPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  };
+  componentDidMount(){
+    this.getLocation();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading></Loading> : null;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// export default function App() {
+//   return <Loading />;
+// }
