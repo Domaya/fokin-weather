1f5cb3d45d0c19e926e587908888df991c24368f
Domaya <c2hila@gmail.com>
2021-08-10 21:51:10 +0900

    ADD Weather API

--- App.js
+++ App.js
@@ -10,15 +10,19 @@ export default class extends React.Component {
   state = {
     isLoading : true
   }
-  // getWeather = async(){
-
-  // }
+  getWeather = async(latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    console.log(data);
+  }
   getLocation = async() => {
     try {
       await Location.requestForegroundPermissionsAsync();
       const {
         coords: { latitude, longitude }
       } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
